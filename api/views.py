63ed6bff19bad0f61a8a50d2144ca3d492a24bcc
dreyfle <norlander163@gmail.com>
2025-11-11 04:50:47 +0800

    updated ping view to not make db connections

--- api/views.py
+++ api/views.py
@@ -22,9 +22,8 @@ from rest_framework.decorators import api_view, permission_classes, action
 from api.utils.encryption import encrypt, decrypt  # Import the decrypt function
 from cryptography.fernet import InvalidToken  # Import the InvalidToken exception
 from django.utils import timezone
-from django.db import models, connection
+from django.db import models
 from django.db.models import Sum, Avg, Max
-from django.db.utils import OperationalError
 import os
 import math
 import logging
@@ -2013,37 +2012,13 @@ def unread_badge_notifications(request):
 
 class PingView(APIView):
     """
-    A simple view to check if the server is alive.
+    A simple view to check if the backend server is alive.
     It's unauthenticated and performs no actions.
     """
     permission_classes = [AllowAny]
 
     def get(self, request, *args, **kwargs):
-        db_connected = False
-        db_error = None
-
-        try:
-            # 1. Get a database connection
-            with connection.cursor() as cursor:
-                # 2. Execute a simple, fast, read-only query.
-                # 'SELECT 1' is standard for this.
-                cursor.execute("SELECT 1")
-            
-            # 3. If we get here, the query was successful
-            db_connected = True 
-        except OperationalError as e:
-            # This is the most common exception Django raises if it can't connect to the database.
-            db_error = str(e)
-
-        if db_connected:
-            # Everything is OK
-            return Response(
-                {"status": "ok", "database": "connected"},
-                status=status.HTTP_200_OK
-            )
-        else:
-            # Database check failed, report a service error
-            return Response(
-                {"status": "error", "database": "disconnected", "error_message": db_error},
-                status=status.HTTP_503_SERVICE_UNAVAILABLE  # 503 Service Unavailable
-            )
+        return Response(
+            {"status": "ok", "server": "running"},
+            status=status.HTTP_200_OK
+        )
